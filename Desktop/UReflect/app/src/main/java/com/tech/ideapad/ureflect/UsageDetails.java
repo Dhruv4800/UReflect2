243cbd073f391d48a4f0db10b8ae0c4e6ff46009
dhruv patel <pateldhruv4800@gmail.com>
2017-04-16 17:39:48 +1000

    v1

---
+++ Desktop/UReflect/app/src/main/java/com/tech/ideapad/ureflect/UsageDetails.java
@@ -0,0 +1,14 @@
+package com.tech.ideapad.ureflect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UsageDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usage_details);
+
+    }
+}
